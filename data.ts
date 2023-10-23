2fd65ee5716bbd0e8a312737fd5752018a5777ef
Maria Ospina <mp.ospinap1@uniandes.edu.co>
2023-10-22 23:11:38 -0500

    Terminado

--- data.ts
+++ data.ts
@@ -6,22 +6,22 @@ import {Serie} from "./serie.js"
 
 //Crear lista de series
 export const series = [new Serie(1,"Breaking Bad","AMC", 5,"Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer" ,
-"https://www.amc.com/shows/breaking-bad","https://i.imgur.com/GGje0vc.jpg"),
+"https://www.amc.com/shows/breaking-bad","https://tierragamer.com/wp-content/uploads/2018/11/Breaking_Bad.jpg"),
 
     new Serie (2,"Orange Is the New Black", "Netflix", 6, "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary", 
-    "https://www.netflix.com/co/title/70242311","https://i.imgur.com/EvKe48G.jpg"),
+    "https://www.netflix.com/co/title/70242311","https://i0.wp.com/www.lapajareramagazine.com/wp-content/uploads/2020/03/orange-is-the-new-black-7-e1564102178946.jpg?resize=678%2C371&ssl=1"),
     
     new Serie (3, "Game of Thrones","HBO", 7, "American fantasy drama", "https://www.hbo.com/game-of-thrones",
-    "https://i.imgur.com/TDCEV1S.jpg"),
+    "https://hips.hearstapps.com/hmg-prod/images/juego-de-tronos-1555075663.jpg?crop=1xw:1xh;center,top&resize=980:*"),
 
     new Serie (4, "The Big Bang Theory", "CBS", 12, "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life. Despite their on-again, off-again relationship in the past, Leonard and Penny have finally gotten married. Even Sheldon has found a female companion, entering into a relationship agreement with neurobiologist Amy Farrah Fowler, and he recently took their relationship to the next level by marrying her after a long courtship. In their free time, Leonard and Sheldon enjoy fantasy role-playing games with their ever-expanding universe of friends, including fellow scientists Koothrappali, Wolowitz, and Wolowitz’s adorable microbiologist wife, Bernadette, who is adjusting to life with their two children.",
-    "https://www.cbs.com/shows/big_bang_theory/about/", "https://i.imgur.com/uAEpVWk.jpg"),
+    "https://www.cbs.com/shows/big_bang_theory/about/", "https://pbs.twimg.com/media/FTD4HCIWIAIxnRj?format=jpg&name=large"),
     
     new Serie (5, "Sherlock", "BBC",  4, "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman), who has returned from military service in Afghanistan with the Royal Army Medical Corps",
-    "https://www.bbc.co.uk/programmes/b018ttws", "https://i.imgur.com/02B7qhj.jpg"),
+    "https://www.bbc.co.uk/programmes/b018ttws", "https://www.telegraph.co.uk/content/dam/tv/2016/12/29/Sherlock_trans_NvBQzQNjv4Bq1UEfOBrhlcXMt83YVMQJhbT5LfTkYhqvBvjfatlPy1M.jpg?imwidth=960"),
 
     new Serie (6, "A Very English Scandal", "BBC", 2, "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.",
-    "https://www.bbc.co.uk/programmes/p065smy4", "https://i.imgur.com/D4y3DrQ.jpg")]
+    "https://www.bbc.co.uk/programmes/p065smy4", "https://lh3.googleusercontent.com/5rCIRzXTMSb8wVC8f2a9S39DPfHQVxr9gdbCQ_SKPJJW9yTUsSLnF5-JT8kywZgUFhBJYQ=s151")]
 
 //Invocar el elemento html que contiene el html para la tabla series
 let seriesTable: HTMLElement = document.getElementById("series")!;
